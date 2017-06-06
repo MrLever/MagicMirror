a5f47b2decdafbb28a8a315c05ffa1c3d3faff7f
MrLever <mrleverx@gmail.com>
2017-06-06 15:59:01 -0400

    Initial setup

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Magic Mirror</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<div class="content">
+			FUCK
+		</div>
+	</body>
+</html>
