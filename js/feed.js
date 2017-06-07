4d680e39cb26e568770d38028ef1f6a4cd02c61a
Nic <nic@nicsknickknackshack.com>
2017-06-07 00:43:08 -0400

    Begin work on RSS feed

---
+++ js/feed.js
@@ -0,0 +1,10 @@
+$.get("feeds/onion.xml", function (data) {
+    $(data).find("item").each(function () { // or "item" or whatever suits your feed
+        var el = $(this);
+
+        console.log("------------------------");
+        console.log("title      : " + el.find("title").text());
+        console.log("author     : " + el.find("author").text());
+        console.log("description: " + el.find("description").text());
+    });
+});
