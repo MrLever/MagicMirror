0cf1d8659bfe944f45a7f44e06c032a6ce73b641
Tiecoon <luke@bierysbargainbarn.com>
2017-06-07 23:28:23 -0400

    switch to json

--- js/weather.js
+++ js/weather.js
@@ -2,14 +2,12 @@ weather();
 function weather(){
 	jQuery.get('/keys/weather.key', function(data){
 		console.log('tick');
-		CONTENT = fetchFeed("http://samples.openweathermap.org/data/2.5/weather?q=Dunedin,us&mode=xml&appid="+data);
+		CONTENT = fetchFeed("http://api.openweathermap.org/data/2.5/weather?q=Dunedin,us&units=imperial&mode=xml&appid="+data,"json");
 		console.log(CONTENT);
 		check = $(CONTENT).length;
-		console.log(check)
-		if(check == 0){
+		if(check == 0){//request fails first time
 			setTimeout(weather,1000);
 		}
-		console.log("pass");
-		console.log($(check).find("temperature").text());
+		
 	});
 }
