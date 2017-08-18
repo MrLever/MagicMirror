73ad661fdaab9376d1663c991c484fa1b784f904
Luke Biery <luke@bierysbargainbarn.com>
2017-08-18 01:46:26 -0400

    finish basics

--- js/steam.js
+++ js/steam.js
@@ -1,29 +1,39 @@
-function keys(){
+steamkeys()
+var ids
+var key
+var div = document.createElement("div")
+function steamkeys(){
 	console.log('fetch begin')
 	jQuery.get('/keys/steam.key',function(key){
-		init(key)
+		steaminit(key)
 	})
 }
 
-function init(key){
+function steaminit(key){
 	key = key.replace(/[\n\r]/g,"")//acts weird without
-	var ids
 
 	for(var i = 0; i < SETTINGS.steam.ids.length;i++){
 		console.log(SETTINGS.steam.ids[i])
 		ids += SETTINGS.steam.ids[i] + ","
 	}
-
+	setInterval(steamfetch(key),1.2e6);
+}
+function steamfetch(key){
+	console.log("testtttttttttttttt")
 	var content = fetchFeed("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+key+"&steamids="+ids+"&format=xml","json",steam);
-
 }
 
 function steam(data){
-	var div = document.createElement("div")
 	var people = data.query.results.response.players.player
+	console.log("testttttttttttttttttttttttttttttttttttt")
+	div.innerHTML=""
 	for( var i = 0; i < people.length; i++){
-		div.innerHTML+="<span class='person'>"+"<img class='profile' src="+people[i].avatarmedium+">"
-		div.innerHTML+="<span class='name'>" + people[i].personaname+"</span></span><br>"
-		document.getElementById("BR").appendChild(div)
+		online = "offline"
+		if(people[i].personastate>0){
+			online = "online"
+		}
+		div.innerHTML+="<div class='person'>"+"<img class='profile' src="+people[i].avatarmedium+">" + "<span class='"+online+"'>" + people[i].personaname+"</span></div><br>"
+		document.getElementById(SETTINGS.steam.loc).appendChild(div)
 	}
+	console.log("FUCKKKKKK?")
 }
