5d4606e540b60a566eece09641dee24e3a7678fe
Nic <nic@nicsknickknackshack.com>
2017-08-19 22:39:17 -0400

    Unfuck steam module

--- js/steam.js
+++ js/steam.js
@@ -1,9 +1,9 @@
 steamkeys()
-var ids
-var key
-var div = document.createElement("div")
+
 function steamkeys(){
-	console.log('fetch begin')
+	if(SETTINGS.DEBUG == 1)
+		console.log('--- BEGIN STEAM MODULE ---');
+	
 	jQuery.get('/keys/steam.key',function(key){
 		steaminit(key)
 	})
@@ -11,21 +11,31 @@ function steamkeys(){
 
 function steaminit(key){
 	key = key.replace(/[\n\r]/g,"")//acts weird without
-
+	var ids;
+	
+	/* Load css for right align if neccessary */
+	var pos = SETTINGS.steam.loc.charAt(1);
+	if(pos == "R"){
+		var link = document.createElement('link');
+				link.setAttribute('rel', 'stylesheet');
+				link.type = 'text/css';
+				link.href = '/css/steamAlt.css';
+				document.head.appendChild(link);
+	}
 	for(var i = 0; i < SETTINGS.steam.ids.length;i++){
 		console.log(SETTINGS.steam.ids[i])
 		ids += SETTINGS.steam.ids[i] + ","
 	}
-	setInterval(steamfetch(key),1.2e6);
+	setInterval(steamfetch(key, ids),1.2e6);
 }
-function steamfetch(key){
-	console.log("testtttttttttttttt")
+function steamfetch(key, ids){
 	var content = fetchFeed("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+key+"&steamids="+ids+"&format=xml","json",steam);
 }
 
 function steam(data){
-	var people = data.query.results.response.players.player
-	console.log("testttttttttttttttttttttttttttttttttttt")
+	var people = data.query.results.response.players.player;
+	var div = document.createElement("div");
+	div.className = "steamWrapper";
 	div.innerHTML=""
 	for( var i = 0; i < people.length; i++){
 		online = "offline"
@@ -35,5 +45,6 @@ function steam(data){
 		div.innerHTML+="<div class='person'>"+"<img class='profile' src="+people[i].avatarmedium+">" + "<span class='"+online+"'>" + people[i].personaname+"</span></div><br>"
 		document.getElementById(SETTINGS.steam.loc).appendChild(div)
 	}
-	console.log("FUCKKKKKK?")
+	if(SETTINGS.DEBUG == 1)
+		console.log('--- END STEAM MODULE ---');
 }
