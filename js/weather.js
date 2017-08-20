1304fcdad263c579ff2a93682548e36620af0fa5
Nic <nic@nicsknickknackshack.com>
2017-08-19 21:36:11 -0400

    Finish first iteration of weather module

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,12 @@
 /* Credit to Alessio Atzeni for weather icons:
 	http://www.alessioatzeni.com/meteocons/
 */
+/* To truncate weekday names */
 var weatherKey;
 initWeather()
 //api.openweathermap.org/data/2.5/weather?id=2172797
 function initWeather(){
-	
+	document.getElementById(SETTINGS.weather.loc).innerHTML = ""; //Clear old data
 	if(SETTINGS.DEBUG == 1)
 		console.log("--- BEGIN WEATHER MODULE ---");
 
@@ -28,16 +29,29 @@ function initWeather(){
 	});
 }
 function weather(data){
-	
+	if(data == null){
+		console.log("****ERR: NO WEATHER DATA RECIEVIED ****");
+		return;
+	}
+	/* Appends city name regardless of choice */
 	if(SETTINGS.DEBUG == 1)
 		console.log(JSON.stringify(data));
-	/* Make a switch for various weather layouts selected by config mode setting. */
+	
+	$('#' + SETTINGS.weather.loc).append("<div class='currentWeatherWrappper'>" +
+			"<span class='title city'>" +
+				data.query.results.current.city.name +
+			"</span>");
 
 	switch(SETTINGS.weather.mode){
 		case 1: //Current and forecast
 			//$('#' + loc).append(currentWeather(data, system));
 			fetchForecast(data);
 			currentWeather(data);
+			var link = document.createElement('link');
+				link.setAttribute('rel', 'stylesheet');
+				link.type = 'text/css';
+				link.href = '/css/weatherMode2.css';
+			document.head.appendChild(link);
 			break;
 		case 2: //forecast only
 			fetchForecast(data);
@@ -53,13 +67,7 @@ function weather(data){
 function currentWeather(data){
 	var system = (SETTINGS.weather.units == "imperial")?" &degF":" &degC";
 	
-	var content =
-		"<div class='currentWeatherWrappper'>" +
-			"<span class='title city'>" +
-				data.query.results.current.city.name +
-			"</span>" +
-
-			"<div class='subTitle tempLine'>" +
+	var content = "<div class='subTitle tempLine'>" +
 			"<span class='temp'> <span class='weatherIco'>" + fetchImage(data.query.results.current.weather.icon) + "</span>" +
 				Math.round(data.query.results.current.temperature.value) + "<span class='weatherSys'>" + system + "</span></span>" +
 			"</div>" +
@@ -74,39 +82,44 @@ function fetchForecast(){
 	fetchFeed("http://api.openweathermap.org/data/2.5/forecast/daily?id=" + SETTINGS.weather.cityID + "&units=" + SETTINGS.weather.units +
 				"&mode=xml&appid=" + weatherKey, "json", forecast);
 }
-function resolveDay(i){
+function resolveWeatherDay(i){
 	switch(i){
 		case(0):
-			return "Sunday";
+			return "Sun";
 		case(1):
-			return "Monday";
+			return "Mon";
 		case(2):
-			return "Tuesday";
+			return "Tue";
 		case(3):
-			return "Wednesday";
+			return "Wed";
 		case(4):
-			return "Thursday";
+			return "Thu";
 		case(5):
-			return "Friday";
+			return "Fri";
 		case(6):
-			return "Saturday";
+			return "Sat";
 	}
 	return "ERR";
 }
 function forecast(data){
 	if(SETTINGS.DEBUG == 1)
 		console.log(JSON.stringify(data)); 
-	
+	var day;
 	var system = (SETTINGS.weather.units == "imperial")?" &degF":" &degC";
-	var content =
-		"<div class='weatherForecastWrappper'>" +
-			"<div class='foreDay'>" +
-				
-			"</div>" +
-		"</div>";
+	var content = "<div class='weatherForecastWrappper'>";
+			for(var i = 0; i < SETTINGS.weather.days; i++){
+				day = new Date(data.query.results.weatherdata.forecast.time[i].day);
+				content += "<div class='foreDay'>" +
+								"<span class='weekDay subTitle'>" + resolveWeatherDay(day.getDay()) + "</span>" +  
+								"<span class='forecastIco'>" + fetchImage(data.query.results.weatherdata.forecast.time[i].symbol.var) + "</span>" +
+								"<span class='forecastTemp subTitle'><span class='forecastHigh'>" + Math.round(data.query.results.weatherdata.forecast.time[i].temperature.max) + " </span>" +
+								"<span class='forecastLow'>" + Math.round(data.query.results.weatherdata.forecast.time[i].temperature.min) + "</span></span>" +
+							"</div>";
+			}
+		content += "</div>";
 	$('#' + SETTINGS.weather.loc).append(content);
 }
-function fetchImage(imgCode, weatherID){
+function fetchImage(imgCode){
 	var url;
 	switch(imgCode){
 		case "01d":
