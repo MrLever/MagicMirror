21828d0e218cfce5af486c3825278b5865344a58
Nic <nic@nicsknickknackshack.com>
2017-09-18 23:19:17 -0400

    Make weather module prettier

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,23 @@
 /* Credit to Alessio Atzeni for weather icons:
 	http://www.alessioatzeni.com/meteocons/
 */
-/* To truncate weekday names */
 var weatherKey;
-initWeather()
-//api.openweathermap.org/data/2.5/weather?id=2172797
+initWeather();
+
 function initWeather(){
 	document.getElementById(SETTINGS.weather.loc).innerHTML = ""; //Clear old data
 	if(SETTINGS.DEBUG == 1)
 		console.log("--- BEGIN WEATHER MODULE ---");
-
+	
+	var pos = SETTINGS.weather.loc.charAt(1);
+	if(pos == "L"){
+		var link = document.createElement('link');
+				link.setAttribute('rel', 'stylesheet');
+				link.type = 'text/css';
+				link.href = '/css/weatherAlt.css';
+				document.head.appendChild(link);
+	}
+	
 	jQuery.get('/keys/weather.key', function(key){
 			weatherKey = key;
 			
@@ -28,33 +36,22 @@ function initWeather(){
 			/* fetchFeed("http://api.openweathermap.org/data/2.5/weather?q=Dunedin,us&units=imperial&mode=xml&appid="+key,"json",weatherprocess); */
 	});
 }
+
 function weather(data){
 	if(data == null){
 		console.log("****ERR: NO WEATHER DATA RECIEVIED ****");
 		return;
 	}
-	/* Appends city name regardless of choice */
+	
 	if(SETTINGS.DEBUG == 1)
 		console.log(JSON.stringify(data));
 	
-	$('#' + SETTINGS.weather.loc).append("<div class='currentWeatherWrappper'>" +
-			"<span class='subTitle city'>" +
-				data.query.results.current.city.name +
-			"</span>");
-
 	switch(SETTINGS.weather.mode){
 		case 1: //Current and forecast
-			//$('#' + loc).append(currentWeather(data, system));
-			fetchForecast(data);
-			currentWeather(data);
-			var link = document.createElement('link');
-				link.setAttribute('rel', 'stylesheet');
-				link.type = 'text/css';
-				link.href = '/css/weatherMode2.css';
-			document.head.appendChild(link);
+			weatherComboCurrent(data);
 			break;
 		case 2: //forecast only
-			fetchForecast(data);
+			fetchForecast(2);
 			break;
 		case 0: //Only current
 		default:
@@ -64,23 +61,75 @@ function weather(data){
 	}
 	setTimeout(initWeather, 3.6e6);
 }
-function currentWeather(data){
-	var system = (SETTINGS.weather.units == "imperial")?" &degF":" &degC";
-	
-	var content = "<div class='subTitle tempLine'>" +
-			"<span class='temp'> <span class='weatherIco'>" + fetchImage(data.query.results.current.weather.icon) + "</span>" +
-				Math.round(data.query.results.current.temperature.value) + "<span class='weatherSys'>" + system + "</span></span>" +
-			"</div>" +
-			"<span class='subtitle weatherVal'>" + data.query.results.current.weather.value + "</span>" +
-		"</div>";
-	$('#' + SETTINGS.weather.loc).append(content);
-}
-function fetchForecast(){
+function fetchForecast(num){
 	if(SETTINGS.DEBUG == 1)
 		console.log("http://api.openweathermap.org/data/2.5/forecast/daily?id=" + SETTINGS.weather.cityID + "&units=" + SETTINGS.weather.units +
 				"&mode=xml&appid=" + weatherKey);
-	fetchFeed("http://api.openweathermap.org/data/2.5/forecast/daily?id=" + SETTINGS.weather.cityID + "&units=" + SETTINGS.weather.units +
-				"&mode=xml&appid=" + weatherKey, "json", forecast);
+	switch(num){
+		case 1:
+			fetchFeed("http://api.openweathermap.org/data/2.5/forecast/daily?id=" + SETTINGS.weather.cityID + "&units=" + SETTINGS.weather.units +
+				"&mode=xml&appid=" + weatherKey, "json", weatherComboForecast);
+			break;
+		case 2:
+		default:
+			fetchFeed("http://api.openweathermap.org/data/2.5/forecast/daily?id=" + SETTINGS.weather.cityID + "&units=" + SETTINGS.weather.units +
+				"&mode=xml&appid=" + weatherKey, "json", forecastWeather);
+		
+	}
+	
+}
+function weatherComboCurrent(data){
+	if(SETTINGS.DEBUG == 1){
+		console.log(data);
+	}
+	var system = (SETTINGS.weather.units == "imperial")?" &degF":" &degC";
+	$('#' + SETTINGS.weather.loc).append("<div class='weatherTitleLine'>"
+		+ "<span class='subTitle city'>" + data.query.results.current.city.name + "</span>"
+		+ "<span class='weatherIco'>" + fetchImage(data.query.results.current.weather.icon) + "</span>"
+		+ "<span class='subTitle temp'>   " + Math.round(data.query.results.current.temperature.value) + "<span class='weatherSys'>" + system + "</span>"
+	+"</div>");
+	fetchForecast(1);
+}
+function weatherComboForecast(data){
+	if(SETTINGS.DEBUG == 1){
+		console.log(data);
+	}
+	var system = (SETTINGS.weather.units == "imperial")?" &degF":" &degC";
+	var day;
+	var system = (SETTINGS.weather.units == "imperial")?" &degF":" &degC";
+	var content = "<div class='weatherForecastWrappper'>";
+			for(var i = 0; i < SETTINGS.weather.days; i++){
+				day = new Date(data.query.results.weatherdata.forecast.time[i].day);
+				content += "<div class='foreDay'>" +
+								"<span class='weekDay subTitle'>" + resolveWeatherDay(day.getDay()) + "</span>" +  
+								"<span class='forecastIco'>" + fetchImage(data.query.results.weatherdata.forecast.time[i].symbol.var) + "</span>" +
+								"<span class='forecastTemp subTitle'><span class='forecastHigh'>" + Math.round(data.query.results.weatherdata.forecast.time[i].temperature.max) + " </span>" +
+								"<span class='forecastLow'>" + Math.round(data.query.results.weatherdata.forecast.time[i].temperature.min) + "</span></span>" +
+							"</div>";
+			}
+		content += "</div>";
+	$('#' + SETTINGS.weather.loc).append(content);
+}
+function currentWeather(data){
+	var system = (SETTINGS.weather.units == "imperial")?" &degF":" &degC";
+	$('#' + SETTINGS.weather.loc).append("<div class='weatherTitleLine'>"
+		+ "<span class='subTitle city'>" + data.query.results.current.city.name + "</span>"
+		+ "<span class='weatherIco'>" + fetchImage(data.query.results.current.weather.icon) + "</span>"
+		+ "<span class='subTitle temp'>   " + Math.round(data.query.results.current.temperature.value) + "<span class='weatherSys'>" + system + "</span>"
+	+"</div>" 
+	+"<span class='subtitle weatherVal'>" + data.query.results.current.weather.value + "</span>");
+	/* 	$('#' + SETTINGS.weather.loc).append("<div class='weatherTitle'>" +
+			"<span class='subTitle city'>" +
+				data.query.results.current.city.name +
+			"</span>");
+	
+	$(".weatherTitle").append("<div class='subTitle tempLine'>" +
+			"<span class='temp'> <span class='weatherIco'>" + fetchImage(data.query.results.current.weather.icon) + "</span>" +
+				Math.round(data.query.results.current.temperature.value) + "<span class='weatherSys'>" + system + "</span></span>" +
+			"</div></div>");
+	var content = "<span class='subtitle weatherVal'>" + data.query.results.current.weather.value + "</span>" +
+		"</div>";
+	$('#' + SETTINGS.weather.loc).append(content); */
 }
 function resolveWeatherDay(i){
 	switch(i){
@@ -101,9 +150,14 @@ function resolveWeatherDay(i){
 	}
 	return "ERR";
 }
-function forecast(data){
-	if(SETTINGS.DEBUG == 1)
-		console.log(JSON.stringify(data)); 
+function forecastWeather(data){
+	if(SETTINGS.DEBUG == 1){
+		console.log(data);
+	}
+	var system = (SETTINGS.weather.units == "imperial")?" &degF":" &degC";
+	$('#' + SETTINGS.weather.loc).append("<div class='weatherTitleLine'>"
+		+ "<span class='subTitle city'>" + data.query.results.weatherdata.location.name + "</span>"
+	+"</div>"); 
 	var day;
 	var system = (SETTINGS.weather.units == "imperial")?" &degF":" &degC";
 	var content = "<div class='weatherForecastWrappper'>";
@@ -119,84 +173,85 @@ function forecast(data){
 		content += "</div>";
 	$('#' + SETTINGS.weather.loc).append(content);
 }
+
 function fetchImage(imgCode){
-	var url;
+	var code;
 	switch(imgCode){
 		case "01d":
-			url = "B";
+			code = "B";
 			break;
 		case "01n":
-			url = "2";
+			code = "2";
 			break;
 		case "02d":
-			url = "H";
+			code = "H";
 			break;
 		case "02n":
-			url = "4";
+			code = "4";
 			break;
 		case "03d":
-			url = "N";
+			code = "N";
 			break;
 		case "03n":
-			url = "5";
+			code = "5";
 			break;
 		case "04d":
-			url = "Y";
+			code = "Y";
 			break;
 		case "04n":
-			url = "%";
+			code = "%";
 			break;
 		case "09d":
-			url = "R";
+			code = "R";
 			break;
 		case "09n":
-			url = "8";
+			code = "8";
 			break;
 		case "10d":
-			url = "Q";
+			code = "Q";
 			break;
 		case "10n":
-			url = "7";
+			code = "7";
 			break;
 		case "11d":
-			url = "Z";
+			code = "Z";
 			break;
 		case "11n":
-			url = "6";
+			code = "6";
 			break;
 		case "13d":
-			url = "W";
+			code = "W";
 			break;
 		case "13n":
-			url = "#";
+			code = "#";
 			break;
 		case "50d":
-			url = "L";
+			code = "L";
 			break;
 		case "50n":
-			url = "L";
+			code = "L";
 			break;
 		default:
-			url = ")";
+			code = ")";
 			console.log("--- ERR: WEATHER ICO NOT FOUND. ---");
 			break;
 	}
 	/* Override for severe weather
 	switch(weatherID){
 		case (900):
-			url = "images/weather/900.png";
+			code = "images/weather/900.png";
 			break;
 		case (901):
-			url = "images/weather/901.png";
+			code = "images/weather/901.png";
 			break;
 		case (902):
-			url = "images/weather/902.png";
+			code = "images/weather/902.png";
 			break;
 		case (906):
-			url = "images/weather/902.png";
+			code = "images/weather/902.png";
 			break;
 		default:
 			break;
 	} */
-	return url;
+	return code;
 }
